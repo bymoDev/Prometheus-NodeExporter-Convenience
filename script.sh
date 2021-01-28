b27cba48a43e706998ad7507d3de72193c09c748
Timo Bergen <timobe@pm.me>
2021-01-28 23:26:08 +0100

    added root check

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 echo "creating user prome node_exporter"
 useradd --no-create-home --shell /bin/false prome
 useradd --no-create-home --shell /bin/false node_exporter
